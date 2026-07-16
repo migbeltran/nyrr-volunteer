d4dd61ae92449d79b84015f522c6e77f962c5eb2
mb <miguel.c.beltran@gmail.com>
2026-07-16 18:46:34 -0400

    Updated volunteer event URLs

--- check_slots.py
+++ check_slots.py
@@ -19,6 +19,11 @@ from bs4 import BeautifulSoup
 
 URLS = [
     "https://events.nyrr.org/nyrr-team-champions-5m-volunteers",
+    "https://events.nyrr.org/tcs-new-york-city-marathon-training-series-12m-volunteers",
+    "https://events.nyrr.org/nyrr-jersey-city-5k-volunteers",
+    "https://events.nyrr.org/nyrr-staten-island-half-volunteers",
+    "https://events.nyrr.org/nyrr-ted-corbitt-15k-volunteers",
+    "https://events.nyrr.org/vcp-cross-country-2-volunteers",
     # add more event URLs here as needed
 ]
 
